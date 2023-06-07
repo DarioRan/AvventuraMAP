2d790f121d89e366c957bc3e5cbf3238f4a6c040
Emanuele Romito <e.romito3@studenti.uniba.it>
2023-06-07 10:53:48 +0200

    ref: room

--- src/main/java/di/uniba/map/b/adventure/type/CommandGUIType.java
+++ src/main/java/di/uniba/map/b/adventure/type/CommandGUIType.java
@@ -1,5 +1,7 @@
 package di.uniba.map.b.adventure.type;
-
+/**
+ * Enumerazione che rappresenta i tipi di output di un comando.
+ */
 public enum CommandGUIType {
     SHOW_TEXT, CHANGE_BACKGROUND, PLAY_SOUND, ADD_INVENTORY, MOVE, TURN_ON, TURN_OFF, LOAD_GAME, HELP, END, INCREMENT_PB_VALUE, UNKNOWN, START_TIMER, SAVE_GAME, STOP_TIMER
 }
