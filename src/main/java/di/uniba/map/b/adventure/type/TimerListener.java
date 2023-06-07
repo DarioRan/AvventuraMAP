287d01b464039fae35eb26608e1ec30ad9474c96
Julian <julian.pajo@gmail.com>
2023-06-07 10:56:27 +0200

    docs per Client, GameDescription, Server, TimerListener e Utils

--- src/main/java/di/uniba/map/b/adventure/type/TimerListener.java
+++ src/main/java/di/uniba/map/b/adventure/type/TimerListener.java
@@ -1,21 +1,47 @@
 package di.uniba.map.b.adventure.type;
 
-import di.uniba.map.b.adventure.AdventureGameGUI;
-import di.uniba.map.b.adventure.Engine;
 
+import di.uniba.map.b.adventure.Engine;
 import java.io.IOException;
 
+/**
+ * classe che implementa un timer per la progress bar della GUI
+ */
 public class TimerListener extends Thread {
+
+    /**
+     * isRunning boolean che indica se il timer è in esecuzione
+     */
     private volatile boolean isRunning = true;
+
+    /**
+     * progress int che indica il progresso della progress bar
+     */
     private int progress = 0;
+
+    /**
+     * engine Engine che rappresenta il motore di gioco
+     */
     private Engine engine;
+
+    /**
+     * delay int che indica il tempo di attesa tra un incremento e l'altro della progress bar
+     */
     private int delay;
+
+    /**
+     * Costruttore della classe TimerListener
+     * @param engine Engine che rappresenta il motore di gioco
+     */
 
     public TimerListener(Engine engine) {
         this.delay = 3500;
         this.engine = engine;
     }
 
+    /**
+     * Metodo che incrementa il progresso della progress bar
+     */
     @Override
     public void run() {
         while (progress < 100 && isRunning) {
@@ -30,14 +56,26 @@ public class TimerListener extends Thread {
             }
         }
     }
+
+    /**
+     * Metodo che ferma il timer
+     */
     public void stopTimer() {
         isRunning = false;
     }
 
+    /**
+     * Metodo che setta il delay del timer
+     * @param delay int che indica il tempo di attesa tra un incremento e l'altro della progress bar
+     */
     public void setDelay(int delay) {
         this.delay = delay;
     }
 
+    /**
+     * Metodo che restituisce il delay del timer
+     * @return int che indica il tempo di attesa tra un incremento e l'altro della progress bar
+     */
     public int getDelay() {
         return this.delay;
     }
