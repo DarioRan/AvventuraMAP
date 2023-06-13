3afbd1cd5815ef46d4afe591016ebc4ffcad05ad
Julian <julian.pajo@gmail.com>
2023-06-13 22:19:36 +0200

    fix: tolto interfacce inutili

--- src/main/java/di/uniba/map/b/adventure/socket/PluginableServer.java
+++ src/main/java/di/uniba/map/b/adventure/socket/PluginableServer.java
@@ -6,8 +6,27 @@ import di.uniba.map.b.adventure.type.CommandGUIOutput;
 import java.io.IOException;
 import java.sql.SQLException;
 
+/**
+ * Interfaccia che rappresenta un server che può essere esteso con dei plugin
+ */
 public interface PluginableServer {
+    /**
+     * Metodo che si occupa di inviare un comando al client
+     * @param commandGUIOutput Comando da inviare al client
+     * @throws IOException Eccezione lanciata in caso di errore di I/O
+     */
     void sendCommand(CommandGUIOutput commandGUIOutput) throws IOException;
+
+    /**
+     * Metodo che si occupa di inviare risorse al client
+     * @throws IOException Eccezione lanciata in caso di errore di I/O
+     * @throws SQLException Eccezione lanciata in caso di errore di SQL
+     */
     void start() throws IOException, SQLException;
+
+    /**
+     * Metodo che si occupa di impostare la connessione con l'Engine
+     * @param engine Engine con cui si vuole impostare la connessione
+     */
     void setEngine(Engine engine);
 }
